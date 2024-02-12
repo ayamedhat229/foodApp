0d3ff4e49551f3ba2998e6e1c076705332dfd9b8
ayamedhat229 <ayamedhat773@gmail.com>
2024-02-12 18:05:04 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{path:'',redirectTo:'auth',pathMatch:'full'},
+{ path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) }, 
+{ path: 'users', loadChildren: () => import('./users/users.module').then(m => m.UsersModule) }, 
+{ path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
